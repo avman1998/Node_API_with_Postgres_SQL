fdd3bf77694dc66dc2ee032e66908afa787923f5
Aman Verma <avman1998@gmail.com>
2023-08-25 16:08:53 +0530

    completed

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-const pool = require("../db");
+const { pool } = require("../db");
 const handleGetUsers = function (req, res) {
   pool.query("SELECT * from users", (error, result) => {
     if (error) throw error;
@@ -8,27 +8,24 @@ const handleGetUsers = function (req, res) {
 };
 const handleGetUsersById = function (req, res) {
   const id = Number(req.params.id);
-  pool.query(
-    "SELECT * from users where id=$1 and name=$2",
-    [id, "AMAN VERMA"],
-    (error, result) => {
-      if (error) throw error;
+  pool.query("SELECT * from users where id=$1", [id], (error, result) => {
+    if (error) throw error;
 
-      res.status(200).json(result.rows);
-    }
-  );
+    res.status(200).json(result.rows);
+  });
 };
 const handlePostUser = function (req, res) {
   const { name, email } = req.body;
   pool.query(
-    "INSERT INTO USERS(name,email) VALUES($1,$2)",
+    "INSERT INTO USERS(name,email) VALUES($1,$2) RETURNING id",
     [name, email],
     (error, result) => {
+      // console.log("result.rows ", result.rows);
       if (error) throw error;
 
       return res.status(201).json({
         msg: "SUCCESS",
-        id: `${result.rows[0].id}`,
+        id: result.rows[0].id,
       });
     }
   );
@@ -51,12 +48,11 @@ const handleUpdateUserById = function (req, res) {
 };
 const handleDeleteUserById = function (req, res) {
   const id = req.params.id;
-  pool.query("DELETE FROM USER WHERE id=$1", [id], (error, data) => {
+  pool.query("DELETE FROM USERS WHERE id=$1", [id], (error, data) => {
     if (error) throw error;
 
     return res.status(200).json({
       msg: "SUCCESS",
-      key: id,
     });
   });
 };
